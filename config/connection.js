93607e5ebbd25b28d69fca8a17d3bd24e289ab77
Dan Bushong <bushongdan@gmail.com>
2021-02-09 17:01:55 -0600

    config and controllers updated

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,21 @@
-module.exports = connection
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Durango11790',
+  database: 'burgers_db',
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
