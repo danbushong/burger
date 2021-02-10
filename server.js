0f036b6fa6298611e97633d05804d7dec146cfff
Dan Bushong <bushongdan@gmail.com>
2021-02-09 20:52:48 -0600

    try againnn

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-const express = require('express');
-const path = require("path");
-const fs = require("fs");
-const exphbs = require('express-handlebars');
+// Create express connection and run node server
+var express = require("express");
+var exphbs = require("express-handlebars");
+var methodOverride = require("method-override");
 
+var PORT = process.env.PORT || 8080;
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
+// This is a level of abstraction to hide credentials from user
 app.use(express.static('public'));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 
-// Set Handlebars.
-
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// the main page is always displayed
+app.use(methodOverride('_method'));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require('./controllers/burgers_controller.js');
+// Norm is to call it "var = routes"
+var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+// Use express routes defined
+app.use("/", routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, () =>
-  console.log(`Server listening on: http://localhost:${PORT}`)
-);
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
