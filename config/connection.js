f70bfb7d5368af9b8c2a562f77603d294ba02fa4
Dan Bushong <bushongdan@gmail.com>
2021-02-09 21:08:25 -0600

    heroku must be down because I have tried everything possible

--- config/connection.js
+++ config/connection.js
@@ -8,14 +8,6 @@ const connection = mysql.createConnection({
   database: 'burgers_db',
 });
 
-// Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
 
 // Export connection for our ORM to use.
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-var express = require("express");
+const express = require('express');
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
-app.use(express.static("public"));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+// Set Handlebars.
+const exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-var routes = require("./controllers/burger_controller.js");
+// Import routes and give the server access to them.
+const routes = require('./controllers/burgers_controller.js');
 
 app.use(routes);
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
