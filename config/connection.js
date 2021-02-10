1b742b48045c8c3d6388c2c56de77a9e1478581b
Dan Bushong <bushongdan@gmail.com>
2021-02-10 11:50:01 -0600

    adding jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Durango11790',
-  database: 'burgers_db',
-});
 
 
+const connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Durango11790',
+    database: 'burgers_db'
+  })
+}
+
+
+connection.connect();
 module.exports = connection;
